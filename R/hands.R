a513050f2c7e573e26b0f56e0b76f2932cc801e3
paithiov909 <am11.44@gmail.com>
2024-10-19 20:25:40 +0900

    refactor: modify params

--- R/hands.R
+++ R/hands.R
@@ -4,19 +4,18 @@
 #' Note that this param is not vectorized.
 #' @param baopai Character vector. "Dora" indicators.
 #' @param libaopai Character vector. "Ura-dora" indicators.
-#' @param rongpai String. Leave empty if there is no rongpai.
+#' Leave with single NA if there is no libaopai.
+#' @param rongpai String.
+#' Leave with single NA if there is no rongpai.
 #' @param rule List; a rule set.
-#' @param zhuangfeng Integer (0-3); "ba-kaze" pai.
-#' @param menfeng Integer (0-3); "ji-kaze" pai.
-#' @param lizhi Integer (0-2);
-#' 0: no lizhi, 1: lizhi, 2: double-lizhi.
+#' @param zhuangfeng String; "ba-kaze" pai.
+#' @param menfeng String; "ji-kaze" pai.
+#' @param lizhi String; One of "none", "lizhi", or "double-lizhi".
 #' @param yifa Logical; flag for "ippatsu".
 #' @param qianggang Logical; flag for "chankan".
 #' @param lingshang Logical; flag for "rinshan-kaihou".
-#' @param haidi Integer (0-2);
-#' 0: normal, 1: haidimoyue, 2: hedilaoyu.
-#' @param tianhu Integer (0-2);
-#' 0: normal, 1: tianhu, 2: dihe.
+#' @param haidi String; One of "none", "haidimoyue", or "hedilaoyu".
+#' @param tianhu String; One of "none", "tianhe", or "dihe".
 #' @param changbang Integer;
 #' Number of counter sticks that indicates "honba".
 #' @param lizhibang Integer;
@@ -26,21 +25,62 @@
 calc_defen <- function(
   pai, # not vectorized!
   baopai,
-  libaopai,
-  rongpai = "",
+  libaopai = NA,
+  rongpai = NA,
   rule = default_rule(),
-  zhuangfeng = 0L,
-  menfeng = 1L,
-  lizhi = 0L,
+  zhuangfeng = c("z1", "z2", "z3", "z4"),
+  menfeng = c("z2", "z3", "z4", "z1"),
+  lizhi = c("none", "lizhi", "double-lizhi"),
   yifa = FALSE,
   qianggang = FALSE,
   lingshang = FALSE,
-  haidi = 0L,
-  tianhu = 0L,
+  haidi = c("none", "haidimoyue", "hedilaoyu"),
+  tianhe = c("none", "tianhe", "dihe"),
   changbang = 0L,
   lizhibang = 0L
 ) {
+  zhuangfeng <- rlang::arg_match(zhuangfeng)
+  menfeng <- rlang::arg_match(menfeng)
+  lizhi <- rlang::arg_match(lizhi)
+  haidi <- rlang::arg_match(haidi)
+  tianhe <- rlang::arg_match(tianhe)
+
+  zhuangfeng <-
+    switch(zhuangfeng,
+      "z1" = 0L,
+      "z2" = 1L,
+      "z3" = 2L,
+      "z4" = 3L
+    )
+  menfeng <-
+    switch(menfeng,
+      "z2" = 1L,
+      "z3" = 2L,
+      "z4" = 3L,
+      "z1" = 0L
+    )
+  lizhi <-
+    switch(lizhi,
+      "none" = 0L,
+      "lizhi" = 1L,
+      "double-lizhi" = 2L
+    )
+  haidi <-
+    switch(haidi,
+      "none" = 0L,
+      "haidimoyue" = 1L,
+      "hedilaoyu" = 2L
+    )
+  tianhe <-
+    switch(tianhe,
+      "none" = 0L,
+      "tianhe" = 1L,
+      "dihe" = 2L
+    )
+
+  libaopai[is.na(libaopai)] <- ""
   rongpai[is.na(rongpai)] <- ""
+
   skksph_get_defen(
     pai,
     baopai,
@@ -56,7 +96,7 @@ calc_defen <- function(
     qianggang,
     lingshang,
     haidi,
-    tianhu,
+    tianhe, # `tianhu` in C++ side
     changbang,
     lizhibang
   )
