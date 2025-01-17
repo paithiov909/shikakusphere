6e6219dfb613c2213e6a3a403f3e306d4cb3b540
paithiov909 <am11.44@gmail.com>
2025-01-18 01:15:33 +0900

    refactor: tweak for R-devel

--- src/calsht.h
+++ src/calsht.h
@@ -1,8 +1,9 @@
 #ifndef CALSHT_HPP
 #define CALSHT_HPP
 
+#ifndef R_NO_REMAP
 #define R_NO_REMAP
-#define STRICT_R_HEADERS
+#endif
 
 #include <Rcpp.h>
 
@@ -11,13 +12,13 @@
 #include <vector>
 
 class Calsht {
-private:
-  using LVec = Rcpp::IntegerVector; // std::vector<int32_t>;
-  using RVec = Rcpp::IntegerVector; // std::vector<int8_t>;
+ private:
+  using LVec = Rcpp::IntegerVector;  // std::vector<int32_t>;
+  using RVec = Rcpp::IntegerVector;  // std::vector<int8_t>;
   // using Iter = std::vector<RVec>::iterator;
 
-  Rcpp::IntegerMatrix mp1; // std::vector<RVec> mp1;
-  Rcpp::IntegerMatrix mp2; // std::vector<RVec> mp2;
+  Rcpp::IntegerMatrix mp1;  // std::vector<RVec> mp1;
+  Rcpp::IntegerMatrix mp2;  // std::vector<RVec> mp2;
 
 #ifdef THREE_PLAYER
   RVec index1(int n) const;
@@ -25,9 +26,11 @@ private:
   void add1(LVec& lhs, const RVec& rhs, int m) const;
   void add2(LVec& lhs, const RVec& rhs, int m) const;
 
-public:
-  Calsht(const Rcpp::IntegerMatrix& index_s, const Rcpp::IntegerMatrix index_h) : mp1(index_s), mp2(index_h) {}
-  Calsht(Rcpp::IntegerMatrix&& index_s, Rcpp::IntegerMatrix&& index_h) : mp1(index_s), mp2(index_h) {}
+ public:
+  Calsht(const Rcpp::IntegerMatrix& index_s, const Rcpp::IntegerMatrix index_h)
+      : mp1(index_s), mp2(index_h) {}
+  Calsht(Rcpp::IntegerMatrix&& index_s, Rcpp::IntegerMatrix&& index_h)
+      : mp1(index_s), mp2(index_h) {}
   int calc_lh(const int* t, int m) const;
   int calc_sp(const int* t) const;
   int calc_to(const int* t) const;
